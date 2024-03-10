507a27100a6adbd03078f3488096764820642c95
Mike Cellini <mike.cellini@protonmail.com>
2024-03-10 07:23:17 -0500

    Add staticcheck to workflow

--- main.go
+++ main.go
@@ -24,6 +24,10 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() {
+    // this func does nothing and is called nowhere
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
