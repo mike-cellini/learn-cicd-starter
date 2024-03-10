2153cf489133b9feb7a09c19146c2d86f846c696
Mike Cellini <mike.cellini@protonmail.com>
2024-03-09 20:41:43 -0600

    Maybe actually add the tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+    "net/http"
+    "testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+    header := http.Header { }
+
+    header.Set("Authorization", "ApiKey 12345")
+    got, err := GetAPIKey(header)
+    want := "12345"
+    if err != nil {
+        t.Fatalf("expected: %v, got: %v", want, err.Error())
+    } else if want != got {
+        t.Fatalf("expected: %v, got: %v", want, got)
+    }
+}
